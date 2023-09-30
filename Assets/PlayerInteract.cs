415c42a001830b325f8a3cf88a91be34a5272cf9
Okan Ataç <okanatac.cl@gmail.com>
2023-09-30 04:19:52 +0300

    base interaction

---
+++ Assets/PlayerInteract.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public KeyCode interactKey = KeyCode.E;
+    private Camera _camera;
+    private bool _isCameraNull;
+    private float _maxDistance;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _isCameraNull = _camera == null;
+    }
+
+    private void Start()
+    {
+        _maxDistance = 500f;
+    }
+
+    private void Update()
+    {
+        // Raycast from your player's camera to detect the interactable object.
+        if (!Input.GetKeyDown(interactKey)) return;
+        if (_isCameraNull) return;
+        if (!Physics.Raycast(_camera.transform.position, _camera.transform.forward, out RaycastHit hit,
+                _maxDistance)) return;
+        var interactable = hit.collider.GetComponent<IInteractable>();
+        interactable?.Interact();
+    }
+}
